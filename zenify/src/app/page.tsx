6836817406cfe775528a9c619e20d474fcdec8b4
DavidDerama <davidbrentderama@gmail.com>
2025-09-08 20:28:40 +0300

    layouts started

--- zenify/src/app/page.tsx
+++ zenify/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <h1>dasasd</h1>
-    </div>
+    <>
+      <h1>Zenify</h1>
+    </>
   );
 }
---
+++ zenify/src/components/layouts/Section.tsx
@@ -0,0 +1,50 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+type SectionProps = {
+  className?: string;
+  children: ReactNode;
+  spacing?: "sm" | "base" | "lg";
+  outerLayer?: boolean;
+  outerClassName?: string;
+};
+
+export default function Section({
+  children,
+  className,
+  spacing,
+  outerLayer = false,
+  outerClassName,
+}: SectionProps) {
+  if (!outerLayer) {
+    return (
+      <section
+        className={cn(
+          "w-full mx-auto max-w-screen-2xl px-6",
+          spacing === "sm" && "py-10",
+          spacing === "base" && "py-20",
+          spacing === "lg" && "py-28",
+          className
+        )}
+      >
+        {children}
+      </section>
+    );
+  }
+
+  return (
+    <section className={outerClassName}>
+      <div
+        className={cn(
+          "w-full mx-auto max-w-screen-2xl px-6",
+          spacing === "sm" && "py-10",
+          spacing === "base" && "py-20",
+          spacing === "lg" && "py-28",
+          className
+        )}
+      >
+        {children}
+      </div>
+    </section>
+  );
+}
